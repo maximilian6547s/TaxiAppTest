1d4e2382eddbe0be71325fbe4b3b733096dd5f2b
Maximilian6547s <maximilian.s517@gmail.com>
2020-04-17 12:42:47 +0300

    Initial commit

---
+++ app/src/main/java/com/example/taxiapptest/DriverSignInActivity.java
@@ -0,0 +1,15 @@
+package com.example.taxiapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DriverSignInActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_driver_sign_in);
+
+    }
+}
